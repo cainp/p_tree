4b435b9f9648da73625ff6ba7548bf7d38de4c2b
cainp <cainapessoa@gmail.com>
2025-11-11 21:17:08 -0300

    corrigindo gerador de vetores

--- ordenacao.c
+++ ordenacao.c
@@ -1,8 +1,9 @@
 #include "ordenacao.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "config.h"
+
+
 // verifica se tem numero repetido
 int ja_existe(int vetor[], int tamanho, int valor_novo) {
     for (int k = 0; k < tamanho; k++) {
@@ -18,12 +19,14 @@ void gerar_e_copiar_vetor(int vetor_original[], int vetor_copia[]) {
     int num_sorteado;
 
     while (i < TAMVT) {
-        num_sorteado = rand() % VMAX + 1;
-    }
-    if (ja_existe(vetor_original, i, num_sorteado == 0)) {
-        vetor_original[i] = num_sorteado;
-        vetor_copia[i] = num_sorteado;
-        i++;
+        num_sorteado = (rand() % VMAX) + 1;
+
+        if (ja_existe(vetor_original, i, num_sorteado) == 0) {
+            vetor_copia[i] = num_sorteado;
+            vetor_original[i] = num_sorteado;
+            i++;
+        }
 
     }
 }
+
